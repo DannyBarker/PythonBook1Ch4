63d1b48fcd4dc8a5b29bb53d8ce642b09bab06b2
Danny Barker <barkerdanny17@yahoo.com>
2019-08-26 15:25:18 -0500

    finished dictionary excercise

---
+++ dictionaryOfWords.py
@@ -0,0 +1,13 @@
+word_definitions = dict()
+
+word_definitions["Curt"] = "guys with an eye infection who also suck at coup"
+word_definitions["Drew"] = "says he lives in memphis, but actually lives with his parents"
+word_definitions["Dustin"] = "old man"
+word_definitions["Danny"] = "G.O.A.T"
+
+
+print(word_definitions["Curt"])
+print(word_definitions["Dustin"])
+
+for key,values in word_definitions.items():
+    print(f"The definition of {key} is {values}")
